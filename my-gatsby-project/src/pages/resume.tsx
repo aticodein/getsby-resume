5cb4d0714570900c77c38ac0415e2c84989a440d
Attila Szaloki <45002010+aticodein@users.noreply.github.com>
2022-02-20 20:18:24 +0000

    Update resume.tsx

--- my-gatsby-project/src/pages/resume.tsx
+++ my-gatsby-project/src/pages/resume.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
+import ContactInfo from 'components/ContactInfo';
 import Experience from 'components/Experience';
 import Education from 'components/Education';
 import Skills from 'components/Skills';
